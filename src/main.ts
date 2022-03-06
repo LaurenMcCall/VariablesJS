265d04d3b80d07ece34bc844b7b360b270e1bf97
Lauren McCall <lmmccolloch@gmail.com>
2022-03-06 13:10:31 -0500

    Done: Practice creating variables

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import "./style.css";
+import './style.css'
+// Practice creating variables
+const numberOfCupsOfCoffee = 0
+const fullName = 'Lauren McCall'
+console.log(`${fullName} drinks ${numberOfCupsOfCoffee} cups of coffee a day`)
+const aboutMe = {
+  fullName: 'Lauren McCall',
+  luckyNumber: 18,
+  favoriteMovies: ['Arrival', 'Encanto', 'Pig', 'Chicago', 'Melancholia'],
+}
