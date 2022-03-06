165e5c4c6202194fe45155d4921085ad8d10dc05
Lauren McCall <lmmccolloch@gmail.com>
2022-03-06 14:04:32 -0500

    Done: Practice getting input from the user

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import './style.css'
-// Practice creating variables
+// ----------Practice creating variables--------------
 const numberOfCupsOfCoffee = 0
 const fullName = 'Lauren McCall'
-console.log(`${fullName} drinks ${numberOfCupsOfCoffee} cups of coffee a day`)
+console.log(`${fullName}  drinks ${numberOfCupsOfCoffee} cups of coffee a day`)
 const aboutMe = {
   fullName: 'Lauren McCall',
   luckyNumber: 18,
   favoriteMovies: ['Arrival', 'Encanto', 'Pig', 'Chicago', 'Melancholia'],
 }
+console.table(aboutMe)
+
+// ---------Practice getting input from the user-------------
+const userName = window.prompt('What is your name?')
+// QUESTION: why is userName not same color as above interpolation?
+console.log('Hello, ${userName}')
