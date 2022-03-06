30ee0a56e7d396d82fc016a1029e7099f0143596
Lauren McCall <lmmccolloch@gmail.com>
2022-03-06 14:29:45 -0500

    Done: Doing math

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 // ----------Practice creating variables--------------
 const numberOfCupsOfCoffee = 0
 const fullName = 'Lauren McCall'
-console.log(`${fullName}  drinks ${numberOfCupsOfCoffee} cups of coffee a day`)
+console.log(`${fullName} drinks ${numberOfCupsOfCoffee} cups of coffee a day`)
 const aboutMe = {
   fullName: 'Lauren McCall',
   luckyNumber: 18,
@@ -12,11 +12,30 @@ console.table(aboutMe)
 
 // ---------Practice getting input from the user-------------
 const userName = window.prompt('What is your name?')
-// QUESTION: why is userName not same color as above interpolation?
-console.log('Hello, ${userName}')
+console.log(`Hello, ${userName}`)
 
 // ------------Converting string input from the user-----------
 const firstNumber = window.prompt('Enter a number.')
 const secondNumber = window.prompt('Enter another number.')
 const firstOperand = Number(firstNumber)
 const secondOperand = Number(secondNumber)
+
+// ----------Doing math -----------------
+const sum = firstOperand + secondOperand
+const difference = firstOperand - secondOperand
+const product = firstOperand * secondOperand
+const quotient = firstOperand / secondOperand
+const remainder = firstOperand % secondOperand
+console.log(`If you add ${firstOperand} and ${secondOperand} you get ${sum}`)
+console.log(
+  `If you subtract ${secondOperand} from ${firstOperand} you get ${difference}`
+)
+console.log(
+  `If you multiply ${firstOperand} and ${secondOperand} you get ${product}`
+)
+console.log(
+  `If you divide ${firstOperand} by ${secondOperand} you get ${quotient}`
+)
+console.log(
+  `If you divide ${firstOperand} by ${secondOperand} you get a remainder of ${remainder}`
+)
